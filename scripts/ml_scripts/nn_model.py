38e3ce53aab8c2022b18fa765dc5c758b4fe4e2f
John Lee <johndlee2009@gmail.com>
2022-09-01 22:30:49 -0400

    ml update

--- scripts/ml_scripts/nn_model.py
+++ scripts/ml_scripts/nn_model.py
@@ -5,6 +5,8 @@ import torch.utils.data as tdata
 import pytorch_lightning as pl
 import numpy as np
 import os
+from pytorch_lightning.callbacks import ModelCheckpoint
+
 
 class BasicNN(nn.Module):
     
@@ -47,7 +49,13 @@ class LightningNN(pl.LightningModule):
         loss = nn.functional.mse_loss(y_hat, y)
         self.log("val_loss", loss)
         return loss
-
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = nn.functional.mse_loss(y_hat, y)
+        self.log("test_loss", loss)
+        return loss
     
     def configure_optimizers(self):
         optimizer = optim.SGD(self.parameters(), lr=self.lr, momentum = .9)
@@ -76,20 +84,39 @@ if __name__ == '__main__':
 
     sim_dataset = Dataset0D(join('training_data/input.npy'), join('training_data/output.npy'))
     
-    #! Can change this to 3 sections for a test set too
+    
+    
     train_len = int(.8 * len(sim_dataset))
-    val_len = len(sim_dataset) - train_len
-    train_dataset, val_dataset = tdata.random_split(sim_dataset, lengths=[train_len, val_len])
+    val_len = int(.1 * len(sim_dataset))
+    test_len = len(sim_dataset) - train_len - val_len
+    train_dataset, val_dataset, test_dataset = tdata.random_split(sim_dataset, lengths=[train_len, val_len, test_len])
     
     
     train_loader = tdata.DataLoader(train_dataset, batch_size = 128, shuffle = True)
-    val_loader = tdata.DataLoader(val_dataset, batch_sampler=128, shuffle = True)
+    val_loader = tdata.DataLoader(val_dataset, batch_sampler=128, shuffle = False, )
+    test_loader = tdata.DataLoader(test_dataset, batch_sampler=128, shuffle = False)
+    
     
     # retrieve first value of Dataset for sizes
     input_data, output_data = sim_dataset[0]
     # Arbitrary
-    nnmodel = BasicNN(input_neurons=len(input_data), output_neurons=len(output_data), hidden_layers=10, neurons_per_layer=30)
+    nnmodel = BasicNN(input_neurons=len(input_data), output_neurons=len(output_data), hidden_layers=10, neurons_per_layer=100)
     litmodel = LightningNN(nnmodel, lr = 1e-5)
     
-    trainer = pl.Trainer( max_epochs=100, accelerator="auto")
+    all_results_folder = join('training_results')
+    if not os.path.exists(all_results_folder):
+        os.mkdir(all_results_folder)
+    
+    cur_results_folder = join('training_results/run1')
+    if not os.path.exists(cur_results_folder):
+        os.mkdir(cur_results_folder)
+    
+    # checkpointing
+    # Init ModelCheckpoint callback, monitoring 'val_loss'
+    checkpoint_callback = ModelCheckpoint(monitor="val_loss")
+
+    trainer = pl.Trainer( max_epochs=100, accelerator="gpu", default_root_dir=cur_results_folder, callbacks=[checkpoint_callback], fast_dev_run=True)
     trainer.fit(model=litmodel, train_dataloaders=train_loader, val_dataloaders=val_loader)
+    
+    
+    trainer.test(model=litmodel, dataloaders=test_loader, ckpt_path='best', verbose = True)
