a2d4f69fa5a8590065d9cdfbb2462aac30b51857
John Lee <johndlee2009@gmail.com>
2022-09-16 14:48:00 -0400

    sobol change

--- scripts/ml_scripts/sobol_sampling_healthy.py
+++ scripts/ml_scripts/sobol_sampling_healthy.py
@@ -1,7 +1,7 @@
 # File: sobol_sampling_healthy.py
 # File Created: Friday, 19th August 2022 4:22:32 pm
 # Author: John Lee (jlee88@nd.edu)
-# Last Modified: Friday, 16th September 2022 12:18:34 pm
+# Last Modified: Friday, 16th September 2022 2:47:45 pm
 # Modified By: John Lee (jlee88@nd.edu>)
 # 
 # Description: Use Sobol sampling to retrieve a distribution of potential diameter changes for a particular healthy model. Takes in an artificial stenosis directory.
@@ -17,6 +17,7 @@ import pandas as pd
 import numpy as np
 from scipy.stats.qmc import Sobol, scale
 import copy
+from pathlib import Path
 from svzerodsolver.runnercpp import run_from_config
 
 
@@ -81,80 +82,93 @@ def parametrize_stenosis(occlusion):
     return 1, radial_increase
 
 
-def data_gen(dims, occlusions, num_samples_log2 = 3):
+def data_gen(dims, occlusions, num_samples, seed, lower_offset = 0.05):
     
     # generate sampler
     bits = 30 # means 2^30 max number of points... should be sufficient. Can go up to 64
-    if num_samples_log2 > bits:
-        raise ValueError(f'num_samples_log2 > {bits}, resulting in duplicates')
-    sobol_sampler = Sobol(d= dims, scramble = True, bits = bits)
+    if num_samples > 2**bits:
+        raise ValueError(f'num_samples > {2**bits}, resulting in duplicates')
+    sobol_sampler = Sobol(d= dims, scramble = True, bits = bits, seed = seed)
     
     # generate bounds
     lbound = []
     ubound = []
     for occ in occlusions:
         bounds = parametrize_stenosis(occ)
-        lbound = bounds[0] - .05
+        lbound = bounds[0] - lower_offset
         ubound = bounds[1]
     
     # create data
     # Make this a generator that does a max amount at a time
-    return scale(sobol_sampler.random_base2(num_samples_log2), l_bounds=lbound, u_bounds=ubound)
-        
-
-
-        
-
+    return scale(sobol_sampler.random(num_samples), l_bounds=lbound, u_bounds=ubound)
     
-    
+
 def main(args):
     
     for solver_dir in tqdm.tqdm(args.solver_dirs, desc = 'Model'):
         
-        join = lambda file: os.path.join(solver_dir, file)
+        root = Path(solver_dir)
         
         # get the solver/stenosis data
         solver_file = get_solver_path(solver_dir)
         solver_data = read_json(solver_file)
-        stenosis_points = read_json(join("stenosis_vessels.dat"))
+        stenosis_points = read_json(root / "stenosis_vessels.dat")
         
         # retrieve targets (last one of a branch)
         #! Subject to change
         targets = ['V' + str(x[-1]) for x in stenosis_points['all_changed_vessels']]
 
-
+        # construct target directory
+        data_dir = root / 'data'
+        if not os.path.exists(data_dir):
+            os.mkdir(data_dir)
+        
+        
         ray.init(num_cpus=args.num_proc)
         
-        # get data
-        data = data_gen(num_samples_log2=args.num_samples, dims = len(stenosis_points['all_changed_vessels']), occlusions = stenosis_points['occlusions'] )
-        
-        # divide data
-        chunk_size = int(np.ceil(len(data) / args.num_proc))
-        print(chunk_size)
-        start = 0
-        results = []
-        
-        for i in range(args.num_proc):
-            # split data
-            chunked_data = data[start:start + chunk_size]
-            results.append(proc_func.remote(chunked_data, stenosis_points['all_changed_vessels'], solver_data, targets))
-            start+=chunk_size
+        for idx, mode in enumerate(['training_data', 'val_data', 'test_data']):
             
-        results = ray.get(results)
-        
-        i = []
-        o = []
-        for input, output in results:
-            i.append(input)
-            o.append(output)
+            mode_dir = data_dir / mode
+            if not os.path.exists(mode_dir):
+                os.mkdir(mode_dir)
+            
+            # change offset 
+            if mode == 'training_data':
+                lower_offset = 0.05
+            else:
+                lower_offset = 0
+                
+            # get data (with different seeds)
+            data = data_gen(num_samples=args.num_samples, 
+                            dims = len(stenosis_points['all_changed_vessels']), 
+                            occlusions = stenosis_points['occlusions'],
+                            seed = 42 + idx,
+                            lower_offset=lower_offset)
+            
+            # divide data
+            chunk_size = int(np.ceil(len(data) / args.num_proc))
+            start = 0
+            results = []
+            
+            for i in range(args.num_proc):
+                # split data
+                chunked_data = data[start:start + chunk_size]
+                results.append(proc_func.remote(chunked_data, stenosis_points['all_changed_vessels'], solver_data, targets))
+                start+=chunk_size
+            results = ray.get(results)
+            
+            i = []
+            o = []
+            for input, output in results:
+                i.append(input)
+                o.append(output)
 
-        if not os.path.exists(join('training_data')):
-            os.mkdir(join('training_data'))
-        
-        np.save(join('training_data/input.npy'), np.array(np.concatenate(i, axis = 0)))
-        np.save(join('training_data/output.npy'), np.array(np.concatenate(o, axis = 0)))
-        
-        
+
+            
+            np.save(mode_dir / 'input.npy', np.array(np.concatenate(i, axis = 0)))
+            np.save(mode_dir / 'output.npy', np.array(np.concatenate(o, axis = 0)))
+            
+            
         ray.shutdown()
   
 
@@ -163,8 +177,8 @@ if __name__ == '__main__':
     parser = create_tool_parser(desc = 'Use Multi-threading to create data samples in numpy array for machine learning ')
     
     parser.add_argument('-ntrain', dest = 'num_train_samples', default = 1024, type = int, help = 'num_train_samples will be generated for training data. Use a power of 2 to guarentee balance properties. Default: 1024 = 2^10.')
-    parser.add_argument('-nval' dest = 'num_val_samples', default = 1024, help = 'num_val_samples will be generated for validation data. Use a power of 2 to guarentee balance properties. Default: 1024 = 2^10.')
-    parser.add_argument('-n')
+    parser.add_argument('-nval', dest = 'num_val_samples', default = 1024, help = 'num_val_samples will be generated for validation data. Use a power of 2 to guarentee balance properties. Default: 1024 = 2^10.')
+    parser.add_argument('-ntest', dest = 'num_test_samples', default = 1024, help = 'num_test_samples will be generated for testing data. Use a power of 2 to guarentee balance properties. Default: 1024 = 2^10.')
     parser.add_argument('-p', dest = 'num_proc', default = 4, type = int, help = 'number of processes to use')
     
     args = parser.parse_args()
